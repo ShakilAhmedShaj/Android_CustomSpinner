2218a99fc15a76b7fb9897b55cb1ffbc56749425
SHAJ <shakilahmedshaj@gmail.com>
2018-09-21 14:14:27 +0600

    listener added and first toast run fixed and color changed

--- app/src/main/java/com/t3ch/shaj/android_customspinner/MainActivity.java
+++ app/src/main/java/com/t3ch/shaj/android_customspinner/MainActivity.java
@@ -2,7 +2,10 @@ package com.t3ch.shaj.android_customspinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private String[] planetNames;
     private String[] diameter;
     int[] planets = {R.drawable.sun, R.drawable.mercury, R.drawable.venus, R.drawable.earth, R.drawable.mars, R.drawable.jupitar, R.drawable.saturn, R.drawable.uranus, R.drawable.neptune};
+    private boolean isFirstSelection = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +27,24 @@ public class MainActivity extends AppCompatActivity {
 
         CustomAdapter adapter = new CustomAdapter(this, planets, planetNames, diameter);
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (isFirstSelection == true) {
+                    isFirstSelection = false;
+                } else {
+                    Toast.makeText(getApplicationContext(), planetNames[position] + " is Selected", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
